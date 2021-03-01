094e4331543b18900d2a620cf70fdeb81926efe9
Oleh Prypin <oleh@pryp.in>
2021-03-01 01:30:20 +0100

    Upgrade doc dependencies and update usage of mkdocstrings (#12)
    * Enable multi-repo [View source] links
    * Use H1 top-level headings, not H2
        * This integrates nicer with the ToC on the right, but also forces all headings to become larger
        * Some styles had to be dropped or modified due to this
    * Remove some styles that mkdocstrings-crystal itself guarantees

--- gen_doc_stubs.py
+++ gen_doc_stubs.py
@@ -14,13 +14,16 @@ for typ in root.lookup("Athena").walk_types():
         for root_typ in root.types:
             if root_typ.kind == "alias":
                 if root_typ.aliased == typ.abs_id:
-                    f.write(f'::: {root_typ.abs_id}\n\n')
+                    f.write(f'# ::: {root_typ.abs_id}\n\n---\n\n')
 
         # Other special top-level entries that don't have their dedicated page.
         if typ.abs_id == "Athena::Config":
-            f.write(f'::: Athena\n\n')
+            f.write(f'# ::: Athena\n\n---\n\n')
         elif typ.abs_id == "Athena::Validator":
-            f.write(f'::: Assert\n\n')
+            f.write(f'# ::: Assert\n\n---\n\n')
 
         # The actual main entry of the page.
-        f.write(f'::: {typ.abs_id}\n\n')
+        f.write(f'# ::: {typ.abs_id}\n\n')
+
+    if typ.locations:
+        mkdocs_gen_files.set_edit_path(filename, typ.locations[0].url)
